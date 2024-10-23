bacd431b889dd908ee8462cdfd215da2bfea12a5
samarthpathak25 <samarthpathak25126@gmail.com>
2024-10-23 15:39:42 +0530

    update

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// 'use strict';
+ 'use strict';
+
+//  nav toogle
+
+const overlay = document.querySelector("[data-overlay]");
+const navbar = document.querySelector("[data-navbar]");
+const navTogglebtn = document.querySelector("[data-nav-toggle-btn]");
+const navbarLink = document.querySelectorAll("[data-nav-link]");
+
+const navToggleFunc = function ()
+{
+    navTogglebtn.classList.toggle("active");
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+
+}
+
+navTogglebtn.addEventListener("click", navToggleFunc);
+overlay.addEventListener("click", navToggleFunc);
+
+for (let i = 0; i < navbarLink.length; i++ ){
+navbarLink[i].addEventListener("click", navToggleFunc);
+
+}
